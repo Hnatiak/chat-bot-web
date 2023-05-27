05cd2293eda178bf4aa517a5ffde9b45fe685946
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-05-27 21:26:22 +0200

    start bot

---
+++ static/scripts/responses.js
@@ -0,0 +1,49 @@
+function getBotResponse(input) {
+    // Rock paper scissors
+    if (input.toLowerCase() === "rock") {
+        return "paper";
+    } else if (input.toLowerCase() === "paper") {
+        return "scissors";
+    } else if (input.toLowerCase() === "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input.toLowerCase() === "hello") {
+        return "Hello there!";
+    } else if (input.toLowerCase() === "goodbye") {
+        return "Talk to you later!";
+    } else if (input.toLowerCase() === "clear") {
+        // Очистити чат і повернути порожній рядок
+        document.getElementById("chatbox").innerHTML = "";
+        return "The chat has been cleared";
+    } else if (
+        input.toLowerCase() === "how are you?" ||
+        input.toLowerCase() === "how are you doing?" ||
+        input.toLowerCase() === "how are u" ||
+        input.toLowerCase() === "how do you feel?" ||
+        input.toLowerCase() === "how do you feel"
+    ) {
+        return "I'm doing well, thank you!";
+    } else if (input.toLowerCase() === "what's your name?") {
+        return "My name is ChatBot.";
+    } else if (input.toLowerCase() === "tell me a joke") {
+        return "Why don't scientists trust atoms? Because they make up everything!";
+    } else if (input.toLowerCase() === "what is the meaning of life?") {
+        return "The meaning of life is subjective and can vary for each individual.";
+    } else if (input.toLowerCase() === "who is your creator?") {
+        return "I was created by OpenAI.";
+    } else if (input.toLowerCase() === "do you like pizza?") {
+        return "As a bot, I don't have taste buds, but I'm always happy to chat about pizza!";
+    } else if (isUkrainian(input)) {
+        return "Будь ласка, напишіть своє повідомлення англійською мовою. тому що я не розумію української";
+    } else {
+        return "I can't understand you, please write something else.";
+    }
+}
+
+// Функція для перевірки, чи містить рядок українські літери
+function isUkrainian(text) {
+    const ukrainianPattern = /[\u0400-\u04FF]/; // Діапазон кодових точок для українських літер в Unicode
+    return ukrainianPattern.test(text);
+}
